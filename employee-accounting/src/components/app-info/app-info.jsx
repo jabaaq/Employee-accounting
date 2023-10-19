afb515479be5be8b81a82ddb935f776fe92ac2f2
Jaba <jabaqadagishvili4@gmail.com>
2023-10-19 15:25:05 +0400

    modified app-info component + styles added

---
+++ employee-accounting/src/components/app-info/app-info.jsx
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Employee accounting of company N</h1>
+      <h2>Total number of employees: </h2>
+      <h2>The bonus will receive: </h2>
+    </div>
+  );
+};
+
+export { AppInfo };
