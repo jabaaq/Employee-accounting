7d18fed160a919e021a18d36b7beaad0b8c4b4ce
Jaba <jabaqadagishvili4@gmail.com>
2023-10-20 16:52:37 +0400

    modified employees add form component / styles added

--- employee-accounting/src/components/app/app.jsx
+++ employee-accounting/src/components/app/app.jsx
@@ -3,6 +3,7 @@ import { AppInfo } from "../app-info/app-info";
 import { SearchPanel } from "../search-panel/search-panel";
 import { AppFilter } from "../app-filter/app-filter";
 import { EmployeesList } from "../employees-list/employees-list";
+import { EmployeesAddForm } from "../employees-add-form/employees-add-form";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <AppFilter />
       </div>
       <EmployeesList />
+      <EmployeesAddForm />
     </div>
   );
 }
