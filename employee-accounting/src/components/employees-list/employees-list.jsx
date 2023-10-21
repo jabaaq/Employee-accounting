b5529684bb2c9ec10e70eba573cfaf4883d8809b
Jaba <jabaqadagishvili4@gmail.com>
2023-10-21 16:47:16 +0400

    added props

--- employee-accounting/src/components/employees-list/employees-list.jsx
+++ employee-accounting/src/components/employees-list/employees-list.jsx
@@ -4,9 +4,9 @@ import "./employees-list.css";
 function EmployeesList() {
   return (
     <ul className="app-list list-group">
-      <EmployeesListItem />
-      <EmployeesListItem />
-      <EmployeesListItem />
+      <EmployeesListItem name="John Smith" salary={800} />
+      <EmployeesListItem name="Rylee Monroe" salary={3000} />
+      <EmployeesListItem name="Carlie Austin" salary={4000} />
     </ul>
   );
 }
