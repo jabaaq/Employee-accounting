5f4b88dabbcc155d602a178000765cbde81710ea
Jaba <jabaqadagishvili4@gmail.com>
2023-10-21 19:50:36 +0400

    created logic to allocate promoted employees

--- employee-accounting/src/components/employees-list/employees-list.jsx
+++ employee-accounting/src/components/employees-list/employees-list.jsx
@@ -3,7 +3,13 @@ import "./employees-list.css";
 
 function EmployeesList({ data }) {
   const elements = data.map((item) => {
-    return <EmployeesListItem name={item.name} salary={item.salary} />; //I can also use [...item]
+    return (
+      <EmployeesListItem
+        name={item.name}
+        salary={item.salary}
+        increase={item.increase} //I can also use:  {...item}
+      />
+    );
   });
 
   return <ul className="app-list list-group">{elements}</ul>;
