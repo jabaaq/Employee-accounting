07bdc4fd4299af826834ef683d512409fb47042a
Jaba <jabaqadagishvili4@gmail.com>
2023-10-24 14:39:34 +0400

    converted a App function component to a class component, because we can work with data dynamically in React only when it is inside the state

--- employee-accounting/src/components/app/app.jsx
+++ employee-accounting/src/components/app/app.jsx
@@ -1,29 +1,40 @@
-import "./app.css";
+import { Component } from "react";
 import { AppInfo } from "../app-info/app-info";
 import { SearchPanel } from "../search-panel/search-panel";
 import { AppFilter } from "../app-filter/app-filter";
 import { EmployeesList } from "../employees-list/employees-list";
 import { EmployeesAddForm } from "../employees-add-form/employees-add-form";
+import "./app.css";
 
-function App() {
-  const data = [
-    { name: "John Smith", salary: 800, increase: false, id: 1 },
-    { name: "Rylee Monroe", salary: 3000, increase: false, id: 2 },
-    { name: "Carlie Austin", salary: 4000, increase: false, id: 3 },
-  ];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: "John Smith", salary: 800, increase: false, id: 1 },
+        { name: "Rylee Monroe", salary: 3000, increase: false, id: 2 },
+        { name: "Carlie Austin", salary: 4000, increase: false, id: 3 },
+      ],
+    };
+  }
 
-  return (
-    <div className="app">
-      <AppInfo />
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+        <EmployeesList
+          data={this.state.data}
+          deleteById={(id) => console.log(id)}
+        />
+        {/*provided this (deleteById) Prop with a function, now i can use it in employees.list.jsx */}
+        <EmployeesAddForm />
       </div>
-      <EmployeesList data={data} deleteById={(id) => console.log(id)} />
-      {/*provided this (deleteById) Prop with a function, now i can use it in employees.list.jsx */}
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export { App };
