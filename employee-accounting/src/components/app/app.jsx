07cc6404d6087f35c740149a4856c9afbe29a392
Jaba <jabaqadagishvili4@gmail.com>
2023-10-25 15:07:08 +0400

    modified onToggleINcrease funtion to change the state correctly and not to violate the principle of immutability

--- employee-accounting/src/components/app/app.jsx
+++ employee-accounting/src/components/app/app.jsx
@@ -12,7 +12,7 @@ class App extends Component {
     this.state = {
       data: [
         {name: 'John Smith', salary: 800, increase: false, like: true, id: 1},
-        {name: 'Rylee Monroe', salary: 3000, increase: false, like: false, id: 2},
+        {name: 'Rylee Monroe', salary: 3000, increase: true, like: false, id: 2},
         {name: 'Carlie Austin', salary: 4000, increase: false, like: false, id: 3},
       ],
     };
@@ -34,6 +34,7 @@ class App extends Component {
         name: name,
         salary: salary,
         increase: false,
+        like: false,
         id: Math.floor(Math.random() * 1000),
       });
       return {data: newData};
@@ -41,11 +42,31 @@ class App extends Component {
   };
 
   onToggleIncrease = (id) => {
-    console.log(`Increase this ${id}`);
+    this.setState(({data}) => {
+      const index = data.findIndex((elem) => elem.id === id);
+
+      const old = data[index];
+      const newItem = {...old, increase: !old.increase}; // it just takes the value either true or false, changes it to the opposite and writes it to 'increase'
+      const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)]; //split the array 'data' in half and added a new element 'newItem' in the middle
+      return {
+        data: newArr,
+      };
+      /*
+       this.setState(({data}) => ({   //Another way to write the above code
+      data: data.map((item) => {
+        if (item.id === id) {
+          return {...item, increase: !item.increase};   //Returns a new object !
+        }
+        return item;
+      }),
+    }));
+  };
+      */
+    });
   };
 
   onToggleRise = (id) => {
-    console.log(`Rise this ${id}`);
+    console.log(`Like this ${id}`);
   };
 
   render() {
